6da83df11894f37d12b5bde2c641e840756813d2
MOHIT SOAM <mohitsoam@gmail.com>
2022-12-11 19:07:53 +0100

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movie_app_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
