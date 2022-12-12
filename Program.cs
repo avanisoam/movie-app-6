a0c0bbf098c4b28f3793487014fe83fcccf8ad7e
MOHIT SOAM <mohitsoam@gmail.com>
2022-12-12 05:21:15 +0100

    Added MySql support

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using movie_app_6.Data;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<movie_app_6Context>(options =>
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("movie_app_6Context") ?? throw new InvalidOperationException("Connection string 'movie_app_6Context' not found.")));
 
+//https://stackoverflow.com/questions/40059929/cannot-find-the-usemysql-method-on-dbcontextoptions
+//package Pomelo.EntityFrameworkCore.MySql
+
+//builder.Services.AddDbContextPool<movie_app_6Context>(options =>
+//{
+//    var connetionString = builder.Configuration.GetConnectionString("MovieContext");
+//    options.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString));
+//});
+
 builder.Services.AddDbContext<movie_app_6Context>(options =>
     options.UseInMemoryDatabase(databaseName: "MovieDb"));
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
